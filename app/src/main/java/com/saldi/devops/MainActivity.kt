7c288cc731d8f61404edea73ab753067db8209b7
sourabh.saldi <sourabh.saldi@choco.com>
2023-07-23 22:35:31 +0200

    upgrade to java 17

--- app/src/main/java/com/saldi/devops/MainActivity.kt
+++ app/src/main/java/com/saldi/devops/MainActivity.kt
@@ -16,7 +16,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            //this should trigger a build
+            //this should trigger a build, now
             DevopsTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
